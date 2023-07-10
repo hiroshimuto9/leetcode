bbf3af39e397d3b3b32e86f6b0f9a45072abf1ec
hiroshimuto9 <mhi6o461o@gmail.com>
2023-07-10 23:48:53 +0900

    answer 27

---
+++ 27.remove-element.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=27 lang=typescript
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+function removeElement(nums: number[], val: number): number {
+  let i = 0;
+  for (const x of nums) {
+    if (x !== val) {
+      nums[i] = x;
+      i++
+    }
+  }
+  return i
+};
+// @lc code=end
+
