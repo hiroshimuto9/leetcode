7b8e911cd0ef0b82adc15d835ccb875c17fb5a57
hiroshimuto9 <mhi6o461o@gmail.com>
2023-07-16 00:11:15 +0900

    answer 14

---
+++ 14.longest-common-prefix.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=14 lang=typescript
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+function longestCommonPrefix(strs: string[]): string {
+  if (strs.length === 1) return strs[0];
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) return strs[0].substring(0, i);
+    }
+  }
+  return strs[0]
+};
+// @lc code=end
+
