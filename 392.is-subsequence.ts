c42d6244a4ad3dad0cdce20daa8abfe8cc47a777
Hiroshi Muto <hiroshi.muto09@gmail.com>
2023-07-19 12:07:51 +0900

    answer 392

---
+++ 392.is-subsequence.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=392 lang=typescript
+ *
+ * [392] Is Subsequence
+ */
+
+// @lc code=start
+function isSubsequence(s: string, t: string): boolean {
+  let indexCount = 0;
+  for (let i = 0; i < t.length; i++) {
+    if (t[i] === s[indexCount]) {
+      indexCount++
+    }
+  }
+  return indexCount === s.length;
+};
+// @lc code=end
+
