7ab10e1bbaccdf160757c4bbe99a3db90cb91a6d
Hiroshi Muto <mhi6o461o@gmail.com>
2023-08-11 12:44:03 +0900

    answer 189

---
+++ 189.rotate-array.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=189 lang=typescript
+ *
+ * [189] Rotate Array
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+  for (let i = 0; i < k; i++) {
+    const lastNum = nums.pop();
+    nums.unshift(lastNum!)
+  }
+};
+// @lc code=end
+
